15e20889c192fe7522c5e66bd32e3c3a10b7900b
comfyte <rafirizqy@yahoo.com>
2018-08-04 12:51:10 +0700

    optimize page performance on mobile devices

--- js.js
+++ js.js
@@ -1,29 +1,34 @@
 var medq = window.matchMedia("(max-width: 600px)");
 
 function hideYokoso(){
+  // console.log("hideYokoso1");
   $('#pleasewait').animate({
     opacity: 'hide',
     fontSize: '+=0.5em'
   }, 250);
   setTimeout(showWrapper, 250); //0ms delay
-}
-
-function showWrapper(){
-  $('.wrapper').animate({opacity: 'show'}, 500);
   if (medq.matches){
     showBG();
   }
-  else{
-    setTimeout(showShadow, 1000); //500ms delay
-  }
+}
+
+function showWrapper(){
+  // console.log("ShowWrapper2");
+  $('.wrapper').animate({opacity: 'show'}, 500);
+  setTimeout(showShadow, 1000); //500ms delay
 }
 
 function showShadow(){
+  // console.log("showShadow3");
   $('.container-box').css('box-shadow', '0px 0px 100px rgba(0, 0, 0, 0.25)');
-  setTimeout(showBG, 1000); //0ms delay
+  if (medq.matches){}
+  else{
+    setTimeout(showBG, 1000); //0ms delay
+  }
 }
 
 function showBG(){
+  // console.log("showBackground4");
   $('.bg-img').css('opacity', '1');
 }
 
