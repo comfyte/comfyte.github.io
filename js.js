0e7c3077d862bc98a587f80f059f57f46e381a5f
comfyte <rafirizqy@yahoo.com>
2018-08-04 20:08:13 +0700

    revert some changes, optimize animations

--- js.js
+++ js.js
@@ -1,34 +1,29 @@
 var medq = window.matchMedia("(max-width: 600px)");
 
 function hideYokoso(){
-  // console.log("hideYokoso1");
   $('#pleasewait').animate({
     opacity: 'hide',
     fontSize: '+=0.5em'
   }, 250);
   setTimeout(showWrapper, 250); //0ms delay
-  if (medq.matches){
-    setTimeout(showBG, 125);
-  }
 }
 
 function showWrapper(){
-  // console.log("ShowWrapper2");
   $('.wrapper').animate({opacity: 'show'}, 500);
-  setTimeout(showShadow, 1000); //500ms delay
-}
-
-function showShadow(){
-  // console.log("showShadow3");
-  $('.container-box').css('box-shadow', '0px 0px 100px rgba(0, 0, 0, 0.25)');
-  if (medq.matches){}
-  else{
-    setTimeout(showBG, 1000); //0ms delay
+  if (medq.matches){
+    setTimeout(showBG, 500); //0ms delay
+  }
+  else {
+    setTimeout(showShadow, 1000); //500ms delay
   }
 }
 
+function showShadow(){
+  $('.container-box').css('box-shadow', '0px 0px 100px rgba(0, 0, 0, 0.25)');
+  setTimeout(showBG, 1000); //0ms delay
+}
+
 function showBG(){
-  // console.log("showBackground4");
   $('.bg-img').css('opacity', '1');
 }
 
