da023797d22470ed8854041d13334e6e6dc247f8
Farrel R <rafirizqy@yahoo.com>
2019-01-09 23:20:29 +0700

    add changing subtitle

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+var subtitle = [
+  "Indonesian",
+  "CGDCT fan",
+  "16",
+  "USL <3",
+  "HS student",
+  "comfyte",
+  "Tech purist" // initial value on last
+]
+
+var i = 0;
+
+function part0() { // 1s delay before changing subtitle
+  setTimeout(part1, 1000);
+}
+
+function part1() { // turn down opacity before changing
+  $("h2").css("opacity", 0);
+  setTimeout(part2, 500); // duration according to css transition: opacity
+}
+
+function part2() { // change!
+  $("h2").text(subtitle[i]);
+  if (i == subtitle.length-1) {i = 0;}
+  else {i++;}
+  setTimeout(part3, 250); // slight delay before displaying again
+}
+
+function part3() { // display!
+  $("h2").css("opacity", 1);
+  setTimeout(part0, 500); // duration according to css transition
+}
+
 $(window).on('load', function(){
   $("body").css("opacity", "1");
   $("img").css("transform", "unset");
+  setTimeout(part0, 1000); // wait until loaded
 })
